1b2383e09e1dfc2326960843c8169e9ba5950f97
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-03-10 01:37:07 -0800

    Add files via upload

---
+++ arguments.py
@@ -0,0 +1,17 @@
+# def  find_sum(*numbers):
+#   result = 0
+  
+#   for  num in  numbers:
+#     result   = result + num
+#   print("sum",result)
+# find_sum(1,2,3)
+
+# find_sum(120,23)
+
+
+def find_sum(*numbers):
+  result = 0
+  for  num in  numbers:
+      result = result + num
+  print("sum",result)
+find_sum(12,334,56)
